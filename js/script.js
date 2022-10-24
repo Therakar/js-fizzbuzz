3ccd26725d731ba3bf69b4b64b4bafa1c7238f23
Therakar <matteo.pau200@gmail.com>
2022-10-24 16:03:03 +0200

    tramite if seleziona i valori multipli di 3 e gli assegna 'fizz'

--- js/script.js
+++ js/script.js
@@ -3,5 +3,10 @@
 // ciclo for che restituisce i numeri da 1 a 100
 
 for (let i = 1; i <= 100; i++){
-    console.log(i)
+
+    // se un numero è multiplo di 3, quindi se lo divido per 3 mi da resto 0, nella console verrà visulaizzato 'fizz'. 
+    if (i % 3 == 0){
+        console.log(`fizz`);
+    } 
+
 }
