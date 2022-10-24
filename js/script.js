ccd0f864517f29624df6ac32c6f80c2852c39b52
Therakar <matteo.pau200@gmail.com>
2022-10-24 16:19:27 +0200

    corregge errore mette come primo valore del ciclo if else quello che seleziona i numeri che sono multipli di 3 e 5 in modo che non vengano sovrascritti

--- js/script.js
+++ js/script.js
@@ -4,20 +4,21 @@
 
 for (let i = 1; i <= 100; i++){
 
-    // se un numero è multiplo di 3, quindi se lo divido per 3 mi da resto 0, nella console verrà visulaizzato 'Fizz'. 
-    if (i % 3 == 0){
-        console.log(`Fizz`);
+    // se un numero è multiplo di 3 e allo stesso tempo di 5, quindi se lo divido per 3 o per 5 mi da resto 0, nella console verrà visulaizzato 'FizzBuzz'.
+    if ((i % 5 == 0) && (i % 3 == 0)){
+        console.log(`FizzBuzz`);
     } 
     // se un numero è multiplo di 5, quindi se lo divido per 5 mi da resto 0, nella console verrà visulaizzato 'Buzz'. 
     else if (i % 5 == 0){
         console.log(`Buzz`);
     } 
-    // se un numero è multiplo di 3 e allo stesso tempo di 5, quindi se lo divido per 3 o per 5 mi da resto 0, nella console verrà visulaizzato 'FizzBuzz'.
-    else if (i % 15 == 0){
-        console.log(`FizzBuzz`);
+    // se un numero è multiplo di 3, quindi se lo divido per 3 mi da resto 0, nella console verrà visulaizzato 'Fizz'. 
+    else if (i % 3 == 0){
+        console.log(`Fizz`);
     }
     // in tutti gli altri casi viene stampato il numero 
     else{
         console.log(i);
     }
 }
+ 
